bac7ff144075a0b169989a665ba94e598c61c5ca
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-04-21 18:15:01 +0300

    update skill type dto

--- DevBoard.API/internal/dtos/skill_type.go
+++ DevBoard.API/internal/dtos/skill_type.go
@@ -1,6 +1,18 @@
 package dtos
 
+type SkillTypeCreateRequest struct {
+	Name string `json:"name" validate:"required,min=2,max=500"`
+}
+
 type SkillTypeResponse struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type SkillTypeUpdateQuery struct {
+	Id int `uri:"id" binding:"required"`
+}
+
+type SkillTypeUpdateRequest struct {
+	Name string `json:"name" validate:"required,min=2,max=500"`
 }
