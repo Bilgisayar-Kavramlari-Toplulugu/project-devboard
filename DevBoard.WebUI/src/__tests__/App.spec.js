c284e9263475197f49c524ba3c8a3847d6e29a5d
Yusuf Soylu <yusufsoylu1974@gmail.com>
2026-04-24 20:15:48 +0300

    revize-2

--- DevBoard.WebUI/src/__tests__/App.spec.js
+++ DevBoard.WebUI/src/__tests__/App.spec.js
@@ -1,16 +1,13 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('mounts without errors', () => {
     const wrapper = mount(App, {
       global: {
-        stubs: {
-          RouterView: true
-        }
-      }
+        stubs: { RouterView: true },
+      },
     })
     expect(wrapper.exists()).toBe(true)
   })
